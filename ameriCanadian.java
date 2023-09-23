c97300da44a615f263ba4650a2fd4eff42afd31d
Ichabod Crane <beboldb@hotmail.com>
2023-09-22 21:19:37 -0400

    Create ameriCanadian.java

---
+++ ameriCanadian.java
@@ -0,0 +1,50 @@
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+// The two standard imports that cover most useful classes.
+import java.util.*;
+import java.io.*;
+
+// import java.math.*; // BigInteger
+public class Solution //problem name here
+{
+    // These two classes are rather fast for input and output.
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    // throw an IOException here so there is no need to handle errors later
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+        // code here
+        
+        String input = next();
+        while(!input.equals("quit!")){
+            if(input.length() > 4 && input.substring(input.length() - 2).equals("or") && input.indexOf(input.length() - 3) != -1){
+                input = input.substring(0, input.length() - 2) + "our";
+            }
+            out.println(input);
+            input = next();
+        }
+
+
+
+        // out.println(); // print out ans;
+        out.close();
+        //System.exit(0);
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()){ st = new StringTokenizer(in.readLine());
+        return st.nextToken();}
+        return null;
+    }
+
+    // read input as integer
+    static int nextInt() throws IOException {return Integer.parseInt(next());}
+
+    // read input as long
+    static long nextLong() throws IOException {return Long.parseLong(next());}
+
+    // read input as double
+    static double nextDouble() throws IOException {return Double.parseDouble(next());}
+}
